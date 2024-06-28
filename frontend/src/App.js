2d848750b813086d607b0717079a06c15252050a
Long Vu <vucongleo@gmail.com>
2024-06-29 00:32:53 +0700

    handle add script.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
 
 function App() {
+  const [videoURL, setVideoURL] = useState(null);
+  const [subtitles, setSubtitles] = useState([]);
+  const [played, setPlayed] = useState(0);
+  const [playedInput, setPlayedInput] = useState([]);
+
+  const [inputs, setInputs] = useState(['']); // Initial state with one input
+
+  const handleAddInput = () => {
+    setInputs([...inputs, '']);
+    setPlayedInput([...playedInput, played]);
+  };
+
+  const handleInputChange = (index, value) => {
+    const newInputs = [...inputs];
+    newInputs[index] = value;
+    setInputs(newInputs);
+  };
+
+  const handleVideoUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setVideoURL(url);
+    }
+  };
+
+  const handleProgress = ({ playedSeconds }) => {
+    setSubtitles([...subtitles, `Subtitle at ${playedSeconds} seconds`]);
+    setPlayed(playedSeconds)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" accept="video/*" onChange={handleVideoUpload} />
+      {videoURL && (
+        <ReactPlayer
+          url={videoURL}
+          controls
+          onProgress={handleProgress}
+        />
+      )}
+      <div>
+        {inputs.map((input, index) => (
+          <div>
+            <p>{ playedInput[index]}</p>
+            <input
+              key={index}
+              type="text"
+              value={input}
+              onChange={(e) => handleInputChange(index, e.target.value)}
+            />
+          </div>
+          
+        ))}
+        <button onClick={handleAddInput}>Add New Input</button>
+      </div>
     </div>
   );
 }
