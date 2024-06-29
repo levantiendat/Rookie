59ffc62d64656b03f0fe4f93cbabbd12b7b4a0f3
nguyen.an913 <52287427+4d4mworth1844@users.noreply.github.com>
2024-06-30 00:44:59 +0700

    feat: Adding Sidebar, Navbar, Intro page and Music page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Intro from './components/Intro'
 import HomePage from './components/HomePage';
+import MusicRecommendation from './components/MusicRecommendation'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
 
 function App() {
   return (
-    <HomePage />
+    <Router>
+      <nav class="navbar navbar-light bg-light border border-success  ">
+        <h3 class="navbar-brand p-2 h1">Rookie - Audio Generate & Synchronize Template</h3>
+      </nav>
+      <div>
+        <div className="d-flex border">
+          <div className="flex-grow-1">
+
+            <Routes>
+              <Route exact path='/' element={<Intro />}></Route>
+              <Route path="/upload-video" element={<HomePage />} />
+              <Route path="/select-music" element={<MusicRecommendation />} />
+            </Routes>
+          </div>
+          <nav className="vertical-bar nav flex-column bg-light p-3 border border-danger" style={{ minHeight: '100vh', width: '200px' }}>
+            <Link className="nav-link m-2" to="/">
+              <i class="fa-solid fa-house fa-2xl m-3"></i>
+              <h3>Intro</h3>
+            </Link>
+            <Link className="nav-link m-2" to="/upload-video">
+              <i class="fa-solid fa-audio-description fa-2xl m-3"></i>
+              <h3>Video</h3>
+            </Link>
+            <Link className="nav-link m-2" to="/select-music">
+              <i class="fa-solid fa-music fa-2xl m-3"></i>
+              <h3>Album</h3>
+            </Link>
+          </nav>
+        </div>
+      </div>
+
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Intro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Intro() {
+  return (
+    <div className="container mt-1">
+      <h1>Welcome to the Video Management System</h1>
+      <p>This is an application where you can upload videos and add subtitles, as well as select music for your videos.</p>
+    </div>
+  );
+}
