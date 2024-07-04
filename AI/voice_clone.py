095c950e81465b61c9282008059d6849341c0c3d
lmkq <khanhquyng0111@gmail.com>
2024-07-05 00:24:37 +0700

    init audio gen model

---
+++ AI/voice_clone.py
@@ -0,0 +1,19 @@
+from bark import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write as write_wav
+    
+def audio_gen(text_prompt,history_prompt, output_path):
+    # Download and load all models
+    preload_models()
+    audio_array = generate_audio(text_prompt,history_prompt=history_prompt)
+    # Save audio to disk
+    write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
+
+# just 4 testing
+if __name__ == '__main__':
+    text_prompt = """
+        Hello, my name is Suno. And, uh — and I like pizza. [laughs] 
+        But I also have other interests such as playing tic tac toe.
+    """
+    history_prompt = "v2/en_speaker_6"  # Update this as needed
+    output_path = "bark_generation.wav"
+    audio_gen(text_prompt, history_prompt, output_path)
