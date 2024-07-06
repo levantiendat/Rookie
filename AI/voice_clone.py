eb1dca1fa60b4b5121b1eea0d9f5125739e9df91
lmkq <khanhquyng0111@gmail.com>
2024-07-06 17:10:37 +0700

    update voice lib

--- AI/voice_clone.py
+++ AI/voice_clone.py
@@ -8,12 +8,13 @@ def audio_gen(text_prompt,history_prompt, output_path):
     # Save audio to disk
     write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
 
-# just 4 testing
+
+# just 4 testing :D
 if __name__ == '__main__':
     text_prompt = """
-        Hello, my name is Suno. And, uh — and I like pizza. [laughs] 
+        Hello, my name is Suno. And, uh — and I like pizza.
         But I also have other interests such as playing tic tac toe.
     """
-    history_prompt = "v2/en_speaker_6"  # Update this as needed
+    history_prompt = "v2/en_speaker_6"  # update this as needed
     output_path = "bark_generation.wav"
     audio_gen(text_prompt, history_prompt, output_path)
