2aee75be66c81587991076805192d93de370f65c
lmkq <khanhquyng0111@gmail.com>
2024-07-07 02:06:06 +0700

    update UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,53 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Intro from './components/Intro'
+import Intro from './components/Intro';
 import HomePage from './components/HomePage';
-import MusicRecommendation from './components/MusicRecommendation'
-import HashtagRecommendation from './components/HashtagRecommendation'
+import MusicRecommendation from './components/MusicRecommendation';
+import HashtagRecommendation from './components/HashtagRecommendation';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <nav class="navbar navbar-light bg-light border border-success">
-        <h3 class="navbar-brand p-2 h1">Rookie - Audio Generate & Synchronize Template</h3>
-      </nav>
-      <div>
-        <div className="d-flex border">
-          <div className="flex-grow-1">
+      <div className="App">
+        <header className="bg-light p-3 d-flex justify-content-center align-items-center">
+          <div>
+            <Link className="nav-link d-inline-flex align-items-center m-2" to="/">
+              <i className="fa-solid fa-house fa-2xl m-3"></i>
+              <h3 className="m-0">Home</h3>
+            </Link>
+            
+            <Link className="nav-link d-inline-flex align-items-center m-2" to="/upload-video">
+              <i className="fa-solid fa-audio-description fa-2xl m-3"></i>
+              <h3 className="m-0">Audio Generation</h3>
+            </Link>
+            
+            <Link className="nav-link d-inline-flex align-items-center m-2" to="/select-music">
+              <i className="fa-solid fa-music fa-2xl m-3"></i>
+              <h3 className="m-0">Music</h3>
+            </Link>
 
-            <Routes>
-              <Route exact path='/' element={<Intro />}></Route>
-              <Route path="/upload-video" element={<HomePage />} />
-              <Route path="/select-music" element={<MusicRecommendation />} />
-              <Route path='/hashtag-generate' element={<HashtagRecommendation/>}></Route>
-            </Routes>
+            <Link className='nav-link d-inline-flex align-items-center m-2' to="/hashtag-generate">
+              <i className="fa-solid fa-hashtag fa-2xl m-3"></i>
+              <h3 className="m-0">Hashtag Recommendation</h3>
+            </Link>
           </div>
-          <nav className="vertical-bar nav flex-column bg-light p-3 border border-danger" style={{ minHeight: '100vh', width: '200px' }}>
-            <Link className="nav-link m-2" to="/">
-              <i class="fa-solid fa-house fa-2xl m-3"></i>
-              <h3>Intro</h3>
-            </Link>
-            
-            <Link className="nav-link m-2" to="/upload-video">
-              <i class="fa-solid fa-audio-description fa-2xl m-3"></i>
-              <h3>Video</h3>
-            </Link>
-            
-            <Link className="nav-link m-2" to="/select-music">
-              <i class="fa-solid fa-music fa-2xl m-3"></i>
-              <h3>Album</h3>
-            </Link>
 
-            <Link className='nav-link m-2' to="/hashtag-generate">
-              <i class="fa-solid fa-hashtag fa-2xl m-3"></i>
-              <h3>Hashtag</h3>
-            </Link>
-          </nav>
+          <div className="flex-grow-1"></div> 
+        </header>
+
+        <div className="main-content">
+          <Routes>
+            <Route exact path='/' element={<Intro />} />
+            <Route path="/upload-video" element={<HomePage />} />
+            <Route path="/select-music" element={<MusicRecommendation />} />
+            <Route path='/hashtag-generate' element={<HashtagRecommendation />} />
+          </Routes>
         </div>
       </div>
-
     </Router>
   );
 }
 
 export default App;
-
-
--- frontend/src/components/Intro.js
+++ frontend/src/components/Intro.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Intro.css';
+import { Link } from 'react-router-dom';
 
 export default function Intro() {
   return (
-    <div className="container mt-1">
-      <h1>Welcome to the Video Management System</h1>
-      <p>This is an application where you can upload videos and add subtitles, as well as select music for your videos.</p>
+    <div className="home-container">
+      <div className="top-banner">
+        <div className="top-banner-illust"></div>
+        <div className="top-banner-content">
+          <h1 className="top-banner-heading">ROOKIE GENERATOR</h1>
+          <p className="top-banner-body">
+            Rookie - Craft Perfect Videos with Audio, Trending Hashtags, and the Right Music!
+          </p>
+          <Link to="/upload-video" className="start-relay-btn">Get Started</Link>
+        </div>
+      </div>
     </div>
   );
 }
